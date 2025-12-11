33e50be94dab42c56c469fe9be635f1f4ea11350
faisstzera <alexandre.faisst@gmail.com>
2025-12-11 11:31:32 -0300

    implementar mudanças de JSON/path do script nos demais arquivos de execução

--- run-nilcmetrix.sh
+++ run-nilcmetrix.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker stop nilcmetrix
 docker rm nilcmetrix
-docker run -d --name nilcmetrix -p 80:8080 --link pgs_cohmetrix:pgs_cohmetrix --link palavras:palavras -v /opt/coh-metrix-nilc:/opt/text_metrics --restart always nilcmetrix:$1
+docker run -d --name nilcmetrix -p 80:8080 --link pgs_cohmetrix:pgs_cohmetrix --link palavras:palavras -v "$SCRIPT_DIR":/opt/text_metrics --restart always nilcmetrix:$1
--- rundps.sh
+++ rundps.sh
@@ -1,1 +1,3 @@
-docker run -d --name cohmetrixp --link pgs_cohmetrix:pgs_cohmetrix -v /home/sidleal/coh-metrix-nilc:/opt/text_metrics --restart always cohmetrix bash -c "python3 porsimples.py"
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+docker run -d --name cohmetrixp --link pgs_cohmetrix:pgs_cohmetrix -v "$SCRIPT_DIR":/opt/text_metrics --restart always cohmetrix bash -c "python3 porsimples.py"
