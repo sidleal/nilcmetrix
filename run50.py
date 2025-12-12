35bb0520eeedf66daad8fb0472fbd6a2762411b6
faisstzera <alexandre.faisst@gmail.com>
2025-12-12 10:34:30 -0300

    fix imports

--- run50.py
+++ run50.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import text_metrics
 import sys
+import json
+import datetime
+import time
 
 feat_list = ["brunet", "simple_word_ratio", "log_pos_conn_ratio", "flesch", "punctuation_ratio", "idade_aquisicao_std", "dep_distance", "third_person_pronouns", "dalechall adapted", "content_word max", "punctuation_diversity", "sentences_with_one_clause", "familiaridade_std", "content_words_ambiguity", "logic_operators", "syllables_per_content_word", "passive_ratio", "adjunct_per_clause", "aux_plus_PCP_per_sentence", "content_word min", "verbs min", "familiaridade_mean", "nouns_ambiguity", "cau_neg_conn_ratio", "ratio_function_to_content_words", "ratio_coordinate_conjunctions", "adverbs_before_main_verb_ratio", "verbs_max", "sentence_length_min", "indicative_pluperfect_ratio", "sentences_with_four_clauses", "adverbs_diversity_ratio", "sentences_with_three_clauses", "idade_aquisicao_4_55_ratio", "words_per_sentence", "frazier", "easy_conjunctions_ratio", "idade_aquisicao_25_4_ratio", "sentences_with_five_clauses", "honore", "apposition_per_clause", "non_svo_ratio", "adjectives_ambiguity", "participle_verbs", "cau_pos_conn_ratio", "max_noun_phrase", "words", "adjective_diversity_ratio", "sentences_with_six_clauses", "verbs"]
 
@@ -14,9 +17,6 @@ t = text_metrics.Text(raw)
 ret = text_metrics.sentence_metrics.values_for_text(t).as_flat_dict()
 
 if use_json:
-    import json
-    import datetime
-    import time
     
     processing_time = time.time()
     result_data = {
--- runport.py
+++ runport.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import text_metrics
 import sys
+import json
+import datetime
+import time
 
 feat_list = ["adj_arg_ovl", "arg_ovl", "adj_stem_ovl", "stem_ovl", "adj_cw_ovl", "adjacent_refs", "anaphoric_refs", "if_ratio", "or_ratio", "and_ratio", "logic_operators", "negation_ratio", "hypernyms_verbs", "conn_ratio", "add_pos_conn_ratio", "add_neg_conn_ratio", "tmp_pos_conn_ratio", "tmp_neg_conn_ratio", "cau_pos_conn_ratio", "cau_neg_conn_ratio", "log_pos_conn_ratio", "log_neg_conn_ratio", "function_words", "content_words", "pronoun_ratio", "adjective_ratio", "words", "paragraphs", "verbs", "adverbs", "words_per_sentence", "syllables_per_content_word", "sentences_per_paragraph", "noun_ratio", "sentences", "flesch", "ttr", "personal_pronouns", "verbs_ambiguity", "adjectives_ambiguity", "nouns_ambiguity", "adverbs_ambiguity", "mean_noun_phrase", "words_before_main_verb", "min_cw_freq", "cw_freq"]
 
@@ -20,9 +23,6 @@ t = text_metrics.Text(raw)
 ret = text_metrics.CMP_METRICS.values_for_text(t).as_flat_dict()
 
 if use_json:
-    import json
-    import datetime
-    import time
     
     processing_time = time.time()
     result_data = {
