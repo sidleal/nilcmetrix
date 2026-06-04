68ed88f5c3c87ec0139088e3cac20e2ed45198d6
Sidney Leal <sidleal@gmail.com>
2026-06-04 11:32:25 -0300

    upgrade ubuntu from focal to noble

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-# TODO: rename tag to cohmetrix:noble (base is ubuntu:noble now, not focal).
-# Requires updating Dockerfile.nilcmetrix, run*.sh, and tests/.
-docker build -t cohmetrix:focal .
+docker build -t cohmetrix:noble .
